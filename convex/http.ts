3790a0f1c5a92cb103958cf2b00f74d5263f2546
Giuseppe Antonio Corona <59139796+peppineddu5@users.noreply.github.com>
2025-12-27 18:20:25 +0100

    refactor: refactor http

--- convex/http.ts
+++ convex/http.ts
@@ -3,25 +3,24 @@ import { HonoWithConvex, HttpRouterWithHono } from "convex-helpers/server/hono";
 import { ActionCtx } from "./_generated/server";
 import z from "zod";
 import { zValidator } from "@hono/zod-validator";
-import { zStorageId } from "./schema/utils"
+import { zStorageId } from "./schema/utils";
 
 const app: HonoWithConvex<ActionCtx> = new Hono();
 
-
-
 app.get(
   "/character/image/:storageId",
-  zValidator("param", z.object({
-    storageId: zStorageId,
-  })),
+  zValidator(
+    "param",
+    z.object({
+      storageId: zStorageId,
+    }),
+  ),
   async (c) => {
     const { storageId } = c.req.valid("param");
 
     const url = await c.env.storage.getUrl(storageId);
 
-    if (!url) {
-      return c.notFound();
-    }
+    if (!url) return c.notFound();
 
     return c.redirect(url, 302);
   },
